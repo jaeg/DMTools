73d9838b7a08a2f572800e6dbe4b2dcbde3ebd46
John Godsey <johngodsey@gmail.com>
2014-10-28 09:10:07 -0400

    Cleaned up code

--- app.js
+++ app.js
@@ -2,74 +2,61 @@ var app = angular.module('dmTools', []);
 
 app.controller('InitiativeController', ['$scope', function($scope) {
     $scope.initiativeList = new Array();
-    
+
     $scope.incrementCount = 1;
-    
+
     $scope.selectedCharacter = null;
-    
-    $scope.addInitiative = function(){
-        var character = {
-        }
-        
-        if ($scope.isModifier == false || typeof $scope.isModifier === "undefined")
-        {
+
+    $scope.addInitiative = function() {
+        var character = {}
+
+        if ($scope.isModifier == false || typeof $scope.isModifier === "undefined") {
             character.initiative = parseInt($scope.characterInitiative);
-        }
-        else
-        {
-         var roll = Math.ceil(Math.random()*20) + parseInt($scope.characterInitiative) | 0;
-         character.initiative = roll;   
+        } else {
+            var roll = Math.ceil(Math.random() * 20) + parseInt($scope.characterInitiative) | 0;
+            character.initiative = roll;
         }
         character.newInitiative = character.initiative;
-        character.name =$scope.characterName;
-        if ($scope.autoIncrement == true)
-        {
+        character.name = $scope.characterName;
+        if ($scope.autoIncrement == true) {
             character.name = character.name + " " + $scope.incrementCount;
             $scope.incrementCount++;
         }
-        
+
         character.notes = $scope.characterNotes;
         character.status = $scope.characterStatus;
         character.dead = false;
         if ($scope.stats != null)
             character.stats = stats;
-        
-        $scope.initiativeList.push(character);  
+
+        $scope.initiativeList.push(character);
     };
     $scope.deleteDead = function() {
         console.log($scope.initiativeList);
-        for (var i = $scope.initiativeList.length - 1; i >= 0; i--)
-        {
-            if ($scope.initiativeList[i].dead == true)
-            {
-                $scope.initiativeList.splice(i,1);
-            }   
+        for (var i = $scope.initiativeList.length - 1; i >= 0; i--) {
+            if ($scope.initiativeList[i].dead == true) {
+                $scope.initiativeList.splice(i, 1);
+            }
         }
     };
-    
-    $scope.change = function(character)
-    {
+
+    $scope.change = function(character) {
         setTimeout(function() {
             character.initiative = character.newInitiative;
-        }, 10);     
+        }, 10);
     }
-    
-    $scope.updateOrder = function()
-    {
-        for(i = 0; i < $scope.initiativeList.length; i++)
-        {
+
+    $scope.updateOrder = function() {
+        for (i = 0; i < $scope.initiativeList.length; i++) {
             //console.log($scope.initiativeList[i].initiative +","+$scope.initiativeList[i].newInitiative);
             $scope.initiativeList[i].initiative = parseInt($scope.initiativeList[i].newInitiative);
-            
+
         }
-        
+
     }
-    
-    $scope.selectCharacter = function(character)
-    {
+
+    $scope.selectCharacter = function(character) {
         $scope.selectedCharacter = character;
     }
-        
+
 }]); //End Controller
-
-
